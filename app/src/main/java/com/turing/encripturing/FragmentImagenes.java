678753a204dc821ea7729a288bb21448ccd8e35e
Alan Malagón <miris_rock2009@hotmail.com>
2017-09-08 20:58:02 -0500

    Reproductor de video listo
    Reproductor de video básico terminado para presentarlo el 13 de septiembre

--- app/src/main/java/com/turing/encripturing/FragmentImagenes.java
+++ app/src/main/java/com/turing/encripturing/FragmentImagenes.java
@@ -1,12 +1,18 @@
 package com.turing.encripturing;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
 
 
 /**
@@ -18,6 +24,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentImagenes extends Fragment {
+
+    private VideoView reproductor;
+    private MediaController mc;
+    private RelativeLayout.LayoutParams paramsNotFullScreen;
+    private int position = 0;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -54,10 +65,6 @@ public class FragmentImagenes extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -105,4 +112,87 @@ public class FragmentImagenes extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    /**
+     * Método para setear el reproductor y su contenido una vez se creó el fragment y la vista del mismo
+     * @param view
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        reproductor = getView().findViewById(R.id.reproductorVideo);
+        reproductor.setVideoPath("storage/emulated/0/Music/Peggo/Eternal Sunshine of the Spotless Mind - 8 Bit Cinema.mp4");
+        //Listener para aplicar el media controller al tamaño del video una vez que esté cargado
+        reproductor.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    //Listener en caso de que el video se redimensione y posicionar de nuevo el media controller
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mediaPlayer, int i, int i1) {
+                        mc = new MediaController(getActivity());
+                        //IMPORTANTE asignar el media controller al videoView antes de posicionarlo
+                        //de lo contrario se colocará en la parte de abajo de la pantalla sobreponiendose
+                        reproductor.setMediaController(mc);
+                        mc.setAnchorView(reproductor);
+                    }
+                });
+            }
+        });
+        reproductor.start();
+        reproductor.requestFocus();
+    }
+
+    /**
+     * Método para guardar la posición actual del video
+     * @param savedInstanceState
+     */
+    @Override
+    public  void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        //Usamos onSaveInstance para poder almacenar la posición de reproducción del video
+        savedInstanceState.putInt("Position", reproductor.getCurrentPosition());
+        reproductor.pause();
+    }
+
+    /**
+     * Método para recuperar la posición del video cada que se gira la pantalla
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        if(savedInstanceState != null) {
+            //Usamos onRestoreInstanceState para poder reproducir la reproducción del video de la posicion almacenada
+            position = savedInstanceState.getInt("Position");
+            reproductor.seekTo(position);
+
+        }
+    }
+
+    /**
+     * Posible método para poder correr el video en fullscreen, sigue en pruebas para lograrlo dentro de un fragment
+     * por lo que está comentado
+     * @param newConfig
+     */
+    /*
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) //Si la orientación cambia a horizontal
+        {
+            paramsNotFullScreen = (RelativeLayout.LayoutParams) reproductor.getLayoutParams();
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(paramsNotFullScreen);
+            params.setMargins(0,0,0,0);
+            params.height=ViewGroup.LayoutParams.MATCH_PARENT;
+            params.width=ViewGroup.LayoutParams.MATCH_PARENT;
+            params.width=ViewGroup.LayoutParams.MATCH_PARENT;
+            params.addRule(RelativeLayout.CENTER_IN_PARENT);
+            reproductor.setLayoutParams(params);
+        }else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            reproductor.setLayoutParams(paramsNotFullScreen);
+        }
+    }*/
+
 }
